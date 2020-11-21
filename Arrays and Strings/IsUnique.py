39a408bdcdb86676bcda0f75c8f7eea23c718c40
Prabhath Kovuri <prabhath.prasad@ibm.com>
2020-11-21 16:44:49 -0600

    is unique complete

--- Arrays and Strings/IsUnique.py
+++ Arrays and Strings/IsUnique.py
@@ -1,2 +1,18 @@
-def isUnique():
-    pass
+def isUnique(str):
+    
+    if len(str) > 128:  # Assuming the string is an ASCII string
+        return False
+    
+    char_set = [0]  # Initialize to start dynamic length increase
+    
+    for i in str:
+        val = str.index(i)
+        
+        if char_set[val]:
+            return False
+        char_set.insert(val, True)
+    
+    return True
+
+
+print(isUnique('abcdefghijkAa'))
