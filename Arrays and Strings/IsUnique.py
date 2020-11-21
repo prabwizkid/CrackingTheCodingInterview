62e950584d4032d19efa8de397296e06bc1c99c0
Prabhath Kovuri <prabhath.prasad@ibm.com>
2020-11-21 17:05:42 -0600

    Added memory to array

--- Arrays and Strings/IsUnique.py
+++ Arrays and Strings/IsUnique.py
@@ -3,16 +3,16 @@ def isUnique(str):
     if len(str) > 128:  # Assuming the string is an ASCII string
         return False
     
-    char_set = [0]  # Initialize to start dynamic length increase
+    char_set = [0] * 128  # Initialize to start dynamic length increase
     
     for i in str:
         val = str.index(i)
         
         if char_set[val]:
             return False
-        char_set.insert(val, True)
+        char_set[val] = True
     
     return True
 
 
-print(isUnique('abcdefghijkAa'))
+print(isUnique('abcdefghijkAb'))
